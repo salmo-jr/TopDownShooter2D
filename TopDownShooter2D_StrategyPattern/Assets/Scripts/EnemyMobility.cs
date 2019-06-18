e72734f366c9b93ce348f380f56f967f7ac82930
DESKTOP-OUVJOVR\Salmo <salmo.jr@hotmail.com>
2019-06-18 09:49:26 -0300

    adapt to Strategy pattern

---
+++ TopDownShooter2D_StrategyPattern/Assets/Scripts/EnemyMobility.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMobility : MonoBehaviour
+{
+    [SerializeField] private sbyte health;
+    [SerializeField] private float speed;
+
+    private Rigidbody2D rb2;
+    private Vector3 zEulerAngles;
+    
+
+    private void Start()
+    {
+        rb2 = GetComponent<Rigidbody2D>();
+        zEulerAngles = Vector3.zero;
+    }
+    private void FixedUpdate()
+    {
+        zEulerAngles.z = Mathf.Atan2(
+            (GameManager.instance.GetPlayerPosition().y - transform.position.y),
+            (GameManager.instance.GetPlayerPosition().x - transform.position.x)) * Mathf.Rad2Deg - 90;
+        transform.eulerAngles = zEulerAngles;
+        rb2.AddForce(gameObject.transform.up * speed);
+    }
+
+    public void TakeDamage(sbyte damage)
+    {
+        health -= damage;
+        if (health <= 0) Die();
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ TopDownShooter2D_StrategyPattern/Assets/Scripts/PlayerMobility.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMobility : MonoBehaviour
+{
+    public float speed;
+
+    private float moveForward;
+    private Vector3 mouseposition;
+    private Quaternion rot;
+    private Rigidbody2D rb2;
+
+    private void Start()
+    {
+        rb2 = GetComponent<Rigidbody2D>();
+    }
+    // Start is called before the first frame update
+    private void FixedUpdate()
+    {
+        mouseposition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        rot = Quaternion.LookRotation(transform.position - mouseposition, Vector3.forward);
+
+        transform.rotation = rot;
+        transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z);
+        moveForward = Input.GetAxis("Vertical");
+        rb2.AddForce(gameObject.transform.up * moveForward * speed);
+    }
+}
